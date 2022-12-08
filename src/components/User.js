b4405fb6e3fd55c42e6a944002a0a578f6f8322a
Imjaae <dlawodud96@naver.com>
2022-12-08 16:51:17 +0900

    최종수정했다

---
+++ src/components/User.js
@@ -0,0 +1,20 @@
+import CustomButton from "./CustomButton";
+
+function User(props) {
+  return (
+    <div className="square-style">
+      {props.user.age}살-{props.user.name}
+      <CustomButton
+        color="red"
+        onClick={() => {
+          props.handleDelete(props.user.id);
+          console.log(props.handleDelete);
+        }}
+      >
+        삭제하기
+      </CustomButton>
+    </div>
+  );
+}
+
+export default User;
